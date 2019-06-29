3b5e8716533dac3fc5fd349bcf933103432c7fc6
Omar Jbara <omar.jbara2@gmail.com>
2019-06-29 19:36:24 +0200

    - Added base dependencies and dummy query

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,9 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Overblog\GraphQLBundle\OverblogGraphQLBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Overblog\GraphiQLBundle\OverblogGraphiQLBundle::class => ['dev' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Liinkiing\GraphQLMakerBundle\GraphQLMakerBundle::class => ['all' => true],
 ];
